20d8066cdcb138a859fd0ff4ba2b06cb78c3b60a
JmFabian <72355352@certus.edu.pe>
2025-04-25 22:05:45 -0500

    guardate si ya

---
+++ script1.js
@@ -0,0 +1,16 @@
+
+function FCambioColor() {
+    let vTexto1 =document.getElementById("EjmDOM1");
+vTexto1.textContent="texto cambiado con la funcion Fcambiocolor()";
+vTexto1.style.color="yellow";
+vTexto1.insertAdjacentHTML("afterend","texto adicional");
+console.log("texto por consola")
+
+
+}
+
+
+
+
+
+
